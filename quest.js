fd73a5370590be819c18b5d601b961953da15240
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2025-04-26 17:51:40 +0100

    quest script and new quests

---
+++ quest.js
@@ -0,0 +1,37 @@
+import fs from "fs"
+import readline from "readline"
+
+function ask(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  return new Promise(resolve => {
+    rl.question(question, answer => {
+      rl.close()
+      resolve(answer)
+    })
+  })
+}
+
+const userId = await ask("Enter user id: ")
+const decorationName = await ask("Enter decoration name: ")
+
+const token = fs.readFileSync("token.txt", "utf-8").trim()
+
+const res = await fetch(`https://discord.com/api/v10/users/${userId}`, {
+  headers: {
+    "Authorization": token
+  }
+})
+const userData = await res.json()
+
+const assetId = userData.avatar_decoration_data.asset.split("_")[1]
+const skuId = userData.avatar_decoration_data.sku_id
+
+const imageRes = await fetch(`https://cdn.discordapp.com/avatar-decoration-presets/${userData.avatar_decoration_data.asset}.png`)
+fs.writeFileSync(`./decorations/avatar/${skuId}.png`, Buffer.from(await imageRes.arrayBuffer()))
+
+const jsonData = JSON.parse(fs.readFileSync("./json/avatarDecorations.json", "utf8"))
+jsonData.Quests[skuId] = decorationName
+fs.writeFileSync("./json/avatarDecorations.json", JSON.stringify(jsonData, null, 2))
