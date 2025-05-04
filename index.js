67fbe7f9168b26c1d82e72ef282a88b4f216e9cd
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2025-05-04 17:42:20 +0100

    nameplates & speed improvements

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ import fs from "node:fs"
 
 fs.mkdirSync("json", { recursive: true })
 fs.mkdirSync("decorations/avatar", { recursive: true })
+fs.mkdirSync("decorations/nameplate", { recursive: true })
 fs.mkdirSync("decorations/profile", { recursive: true })
 
 const token = fs.readFileSync("token.txt", "utf-8")
 
 const avatars = fs.existsSync("json/avatarDecorations.json") ? JSON.parse(fs.readFileSync("json/avatarDecorations.json")) : {}
+const nameplates = fs.existsSync("json/nameplateDecorations.json") ? JSON.parse(fs.readFileSync("json/nameplateDecorations.json")) : {}
 const profiles = fs.existsSync("json/profileDecorations.json") ? JSON.parse(fs.readFileSync("json/profileDecorations.json")) : {}
 
 const properties = Buffer.from(JSON.stringify({
-  "client_build_number": 256231
+  "client_build_number": 396183
 })).toString("base64")
 
 const categories = await fetch("https://discord.com/api/v9/collectibles-categories", {
@@ -21,6 +23,15 @@ const categories = await fetch("https://discord.com/api/v9/collectibles-categori
   }
 }).then(e => e.json())
 
+const profileEffects = await fetch("https://discord.com/api/v9/user-profile-effects", {
+  headers: {
+    Authorization: token,
+    "X-Super-Properties": properties
+  }
+}).then(e => e.json())
+
+const promises = []
+
 for (const category of categories) {
   if (category.name === "Autumn") {
     category.name = "Fall"
@@ -29,24 +40,30 @@ for (const category of categories) {
   }
   for (const product of category.products) {
     for (const item of product.items) {
-      if (item.type !== 0) continue
-      console.log(`Avatar: ${category.name + " - " + product.name}`)
-      avatars[category.name] ??= {}
-      const avatar = await fetch(`https://cdn.discordapp.com/avatar-decoration-presets/${item.asset}.png`).then(e => e.arrayBuffer())
-      fs.writeFileSync(`decorations/avatar/${item.id}.png`, Buffer.from(avatar))
-      avatars[category.name][item.id] = product.name
+      if (item.type === 0) {
+        avatars[category.name] ??= {}
+        avatars[category.name][item.id] = product.name
+        promises.push(fetch(`https://cdn.discordapp.com/avatar-decoration-presets/${item.asset}.png`).then(async e => {
+          console.log(`Avatar: ${category.name + " - " + product.name}`)
+          fs.promises.writeFile(`decorations/avatar/${item.id}.png`, Buffer.from(await e.arrayBuffer()))
+        }))
+      } else if (item.type === 2) {
+        nameplates[category.name] ??= {}
+        promises.push(
+          fetch(`https://cdn.discordapp.com/assets/collectibles/${item.asset}asset.webm`).then(async e => {
+            console.log(`Nameplate WebM: ${category.name + " - " + product.name}`)
+            fs.promises.writeFile(`decorations/nameplate/${item.id}.webm`, Buffer.from(await e.arrayBuffer()))
+          }),
+          fetch(`https://cdn.discordapp.com/assets/collectibles/${item.asset}static.png`).then(async e => {
+            console.log(`Nameplate PNG: ${category.name + " - " + product.name}`)
+            fs.promises.writeFile(`decorations/nameplate/${item.id}.png`, Buffer.from(await e.arrayBuffer()))
+          })
+        )
+        nameplates[category.name][item.id] = product.name
+      }
     }
   }
 }
-
-fs.writeFileSync("json/avatarDecorations.json", JSON.stringify(avatars, null, 2))
-
-const profileEffects = await fetch("https://discord.com/api/v9/user-profile-effects", {
-  headers: {
-    Authorization: token,
-    "X-Super-Properties": properties
-  }
-}).then(e => e.json())
 
 for (const effect of profileEffects.profile_effect_configs) {
   const match = Object.entries(profiles).find(e => e[0] === effect.id || e[1].title === effect.title)
@@ -64,21 +81,30 @@ for (const effect of profileEffects.profile_effect_configs) {
       extra: []
     }
   }
-  console.log(`Profile: ${effect.title}`)
+  profiles[effect.id] = decoration
   fs.mkdirSync(`decorations/profile/${effect.id}`, { recursive: true })
   for (const img of effect.effects) {
     const name = path.basename(img.src, ".png")
     if (!decoration.effects.includes(name)) decoration.effects.push(name)
-    fs.writeFileSync(`decorations/profile/${effect.id}/${name}.png`, Buffer.from(await fetch(img.src).then(e => e.arrayBuffer())))
+    promises.push(fetch(img.src).then(async e => {
+      console.log(`Profile PNG: ${effect.title}`)
+      fs.promises.writeFile(`decorations/profile/${effect.id}/${name}.png`, Buffer.from(await e.arrayBuffer()))
+    }))
   }
   for (const key of ["staticFrameSrc", "thumbnailPreviewSrc", "reducedMotionSrc"]) {
     if (effect[key]) {
       const name = path.basename(effect[key], ".png")
       if (!decoration.extra.includes(name)) decoration.extra.push(name)
-      fs.writeFileSync(`decorations/profile/${effect.id}/${name}.png`, Buffer.from(await fetch(effect[key]).then(e => e.arrayBuffer())))
+      promises.push(fetch(effect[key]).then(async e => {
+        console.log(`Profile ${key}: ${effect.title}`)
+        fs.promises.writeFile(`decorations/profile/${effect.id}/${name}.png`, Buffer.from(await e.arrayBuffer()))
+      }))
     }
   }
-  profiles[effect.id] = decoration
 }
 
+await Promise.all(promises)
+
+fs.writeFileSync("json/avatarDecorations.json", JSON.stringify(avatars, null, 2))
+fs.writeFileSync("json/nameplateDecorations.json", JSON.stringify(nameplates, null, 2))
 fs.writeFileSync("json/profileDecorations.json", JSON.stringify(profiles, null, 2))
